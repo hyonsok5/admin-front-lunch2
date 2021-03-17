de0048b0632d86c5c0b353cbb5b71e7f593f6cf5
potsn <s@hyonsok.io>
2021-03-17 19:53:12 +0900

    init

---
+++ src/main/java/io/hyonsok/frnt/FrntLunchAdminApplication.java
@@ -0,0 +1,13 @@
+package io.hyonsok.frnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrntLunchAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrntLunchAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hyonsok/frnt/FrntLunchAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hyonsok.frnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrntLunchAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
